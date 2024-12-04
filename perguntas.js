66170a79641e4af149ff59815cfeba70d15984f6
nika-joyboy <00001108115251sp@al.educacao.sp.gov.br>
2024-12-04 17:29:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Jogos Vorazes',
+    'Quem é a protagonista de Jogos Vorazes?',
+    'Katniss Everdeen, uma jovem do Distrito 12 que se torna símbolo de rebelião.'
+)
+
+criaCartao(
+    'Jogos Vorazes',
+    'O que são os Jogos Vorazes?',
+    'Uma competição mortal organizada pela Capital para controlar os distritos.'
+)
+
+criaCartao(
+    'Jogos Vorazes',
+    'Quem acompanha Katniss nos Jogos?',
+    'Peeta Mellark, que também é do Distrito 12 e tem um papel crucial na trama.'
+)
+
+criaCartao(
+    'Jogos Vorazes',
+    'Qual é o tema central da série?',
+    'Luta contra a opressão e as consequências de um governo autoritário.'
+)
